1316902fcd722951a58722c6b6f4b37047e61d4c
Tarun <tarunvelli@gmail.com>
2020-06-13 20:28:39 +0530

    add devise sign in

--- Gemfile
+++ Gemfile
@@ -54,4 +54,7 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
+
+# Added by developer
+gem 'devise'
 gem "react-rails", "~> 2.6"
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'orders#index'
+  devise_for :users
+  root 'users#sign_in'
 
   resources :orders
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
