7db06c09a30ffbfb3d97e62b32c3497cd7a2f450
Tarun <tarunvelli@gmail.com>
2020-06-13 19:34:52 +0530

    initial commit

---
+++ app/views/orders/_order.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! order, :id, :user, :note, :cost, :created_at, :updated_at
+json.url order_url(order, format: :json)
---
+++ db/migrate/20200613125950_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.string :user
+      t.text :note
+      t.float :cost
+
+      t.timestamps
+    end
+  end
+end
