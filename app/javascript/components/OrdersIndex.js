87b9aecefd54eb7b2b653f1cdf8b709c40bdb810
Tarun <tarunvelli@gmail.com>
2020-06-25 22:16:07 +0530

    orders board css

--- app/javascript/components/OrdersIndex.js
+++ app/javascript/components/OrdersIndex.js
@@ -12,40 +12,71 @@ class OrdersIndex extends React.Component {
   }
 
   componentDidMount() {
-    this.cable = actioncable.createConsumer('ws://localhost:3000/cable');
-    this.orderChannel = this.cable.subscriptions.create({
-      channel: `OrdersChannel`,
-      restaurant_id: this.props.restaurant_id
-    },{
-      connected: () => {
-          console.log("connected!")
+    this.cable = actioncable.createConsumer("ws://localhost:3000/cable");
+    this.orderChannel = this.cable.subscriptions.create(
+      {
+        channel: `OrdersChannel`,
+        restaurant_id: this.props.restaurant_id,
       },
-      disconnected: () => {},
-      received: data => {
-        this.setState({
-          orders: data
-        });
+      {
+        connected: () => {
+          console.log("connected!");
+        },
+        disconnected: () => {},
+        received: (data) => {
+          this.setState({
+            orders: data,
+          });
+        },
       }
-    })
-  }
-
-  orderCard = (order) => {
-    return(
-      <div className="card" style={{ margin: '1rem', width: '18rem' }} key={order.id}>
-        <div className="card-body">
-          <h5>From : {order.from}</h5>
-          <h6>Note : {order.note}</h6>
-          <h6>Cost : {order.cost}</h6>
-        </div>
-      </div>
     );
   }
 
-  render () {
-    let orders = this.state.orders.map(order => this.orderCard(order))
+  orderCard = (order) => {
+    return (
+      <div
+        className="shadow-sm card order-card"
+        key={order.id}
+      >
+        <div className="card-body">
+          <h5>Order Id : {order.id}</h5>
+          <h6>From : {order.from}</h6>
+          <h6>Note : {order.note}</h6>
+          <h6>Cost : {order.cost}</h6>
+        </div>
+        <div className='row navigation'>
+          <div className='col-sm-6'>
+            <button type="button" className="btn btn-danger btn-block">
+              <i className="fa fa-fw fa-backward"></i>
+            </button>
+          </div>
+
+          <div className='col-sm-6'>
+            <button type="button" className="btn btn-success btn-block">
+              <i className="fa fa-fw fa-forward"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  render() {
+    let orders = this.state.orders.map((order) => this.orderCard(order));
     return (
       <React.Fragment>
-        { orders }
+        <div className="row">
+          <div className="col-sm-4 orders-column">
+            <h4>Received</h4>
+            {orders}
+          </div>
+          <div className="col-sm-4 orders-column">
+            <h4>Preparing</h4>
+          </div>
+          <div className="col-sm-4 orders-column">
+            <h4>Delivering</h4>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
