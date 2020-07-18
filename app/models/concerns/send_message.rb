6ffdf7a5e91f7ae7c2773e5feca5da2fe85856c1
Tarun <tarunvelli@gmail.com>
2020-07-18 17:03:14 +0530

    send message on update

---
+++ app/models/concerns/send_message.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module SendMessage
+  extend ActiveSupport::Concern
+
+  def send_message
+    if saved_change_to_state?
+      @restaurant = restaurant
+      @client = Twilio::REST::Client.new(
+        @restaurant.account_sid, @restaurant.auth_token
+      )
+
+      state_change = saved_change_to_state
+      if state_change[0] == 'received' && state_change[1] == 'preparing'
+        message = "Order #{id} has been accepted and is being prepared!"
+      elsif Order::PRE_DELIVERY_STATES.include?(state_change[0]) && state_change[1] == 'delivering'
+        message = "Order #{id} is out for delivery!"
+      elsif Order::PRE_DELIVERED_STATES.include?(state_change[0]) && state_change[1] == 'delivered'
+        message = "Order #{id} has been delivered, please enjoy your food!"
+      end
+
+      unless message.nil?
+        @client.messages.create(
+          from: 'whatsapp:+14155238886',
+          body: message,
+          to: "whatsapp:#{from}"
+        )
+      end
+    end
+  end
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,15 @@
 class Order < ApplicationRecord
+  include SendMessage
+
   belongs_to :restaurant
   has_many :order_items
   has_many :items, through: :order_items
 
   after_save :broadcast_orders_channel
+  after_update :send_message
+
+  PRE_DELIVERY_STATES = ['received', 'preparing']
+  PRE_DELIVERED_STATES = PRE_DELIVERY_STATES + ['delivering']
 
   private
   def broadcast_orders_channel
