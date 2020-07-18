265189a8bc7a916da1a44c8c22cf65ae7c62c71d
Tarun <tarunvelli@gmail.com>
2020-07-19 02:56:39 +0530

    sticky header on menu page

--- app/javascript/components/Menu.js
+++ app/javascript/components/Menu.js
@@ -13,25 +13,25 @@ class OrdersIndex extends React.Component {
 
   lineItem = (item) => {
     return (
-      <div className="col-lg-6 menu-item filter-starters">
+      <div key={item.id} className="col-lg-6 menu-item filter-starters">
         <div className="menu-content">
           <span>{item.name}</span>
           <span>$ {item.cost}</span>
         </div>
         <div className="menu-ingredients">{item.description}</div>
-        <div class="d-flex flex-row-reverse">
+        <div className="d-flex flex-row-reverse">
           <div
-            class="btn-group btn-group-thin btn-group-sm mt-2"
+            className="btn-group btn-group-thin btn-group-sm mt-2"
             role="group"
             aria-label="Small button group"
           >
-            <button type="button" class="btn btn-toggle">
+            <button type="button" className="btn btn-toggle">
               -
             </button>
-            <button type="button" class="btn btn-light" disabled>
+            <button type="button" className="btn btn-light" disabled>
               0
             </button>
-            <button type="button" class="btn btn-toggle">
+            <button type="button" className="btn btn-toggle">
               +
             </button>
           </div>
@@ -60,10 +60,20 @@ class OrdersIndex extends React.Component {
   render() {
     return (
       <React.Fragment>
+        <nav className="d-flex navbar flex-row-reverse navbar-expand-md navbar-dark fixed-top">
+          <form className="form-inline mt-2 mt-md-0">
+            <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit"
+            >
+              Checkout <i class="fa fa-shopping-cart"></i>
+            </button>
+          </form>
+        </nav>
         <main id="main">
-          <section id="menu" class="menu">
-            <div class="container">
-              <div class="section-title">
+          <section id="menu" className="menu">
+            <div className="container">
+              <div className="section-title">
                 <h2>
                   Check our tasty <span>Menu</span>
                 </h2>
@@ -75,9 +85,9 @@ class OrdersIndex extends React.Component {
           </section>
         </main>
         <footer id="footer">
-          <div class="container">
-            <h3> { this.props.restaurant_name } </h3>
-            <div class="credits">
+          <div className="container">
+            <h3> {this.props.restaurant_name} </h3>
+            <div className="credits">
               All the links in the footer should remain intact. You can delete
               the links only if you purchased the pro version. Licensing
               information: https://bootstrapmade.com/license/ Purchase the pro
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,9 +9,12 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 require('bootstrap')
 require('../stylesheets/application.scss')
 
+window.jQuery = $;
+window.$ = $;
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
