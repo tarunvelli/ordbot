794668a51b03b6cadebe8e1273485a17b52dc5fe
Tarun <tarunvelli@gmail.com>
2020-07-20 21:49:49 +0530

    edit profile page

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -2,6 +2,6 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   private
   def after_confirmation_path_for(resource_name, resource)
     sign_in(resource) # In case you want to sign in the user
-    new_restaurant_path
+    root_path
   end
 end
