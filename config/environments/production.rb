0366358a9fb0fb5afa6cd4380716c9b252fcba9e
Tarun <tarunvelli@gmail.com>
2020-07-21 01:04:13 +0530

    production actionmailer dummy settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "whatsapp_restaurant_bot_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.default_options = {from: 'whatsapporderbot@gmail.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV['GMAIL_USER'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
