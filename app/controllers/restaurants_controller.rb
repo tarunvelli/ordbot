52421cc4eaf473b4cc20dd47aaaeebe5ea3b8b3d
Tarun <tarunvelli@gmail.com>
2020-07-24 23:30:58 +0530

    redirect to edit page with instructions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -29,7 +29,7 @@ class RestaurantsController < PanelController
 
     respond_to do |format|
       if @restaurant.save
-        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully created.' }
+        format.html { redirect_to edit_restaurant_path(@restaurant), notice: 'Restaurant was successfully created. Please paste the Webhook url in Twilio.' }
         format.json { render :show, status: :created, location: @restaurant }
       else
         format.html { render :new }
