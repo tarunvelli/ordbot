ad6420135ee702b8bf24f0ede38206a93bf69a1b
Tarun <tarunvelli@gmail.com>
2020-08-27 14:13:13 +0530

    add github link

--- app/javascript/components/menu/footer.js
+++ app/javascript/components/menu/footer.js
@@ -6,12 +6,14 @@ const Footer = (props) => {
     <footer id="footer">
       <div className="container">
         <h3> {props.restaurantName} </h3>
-        <div className="credits">
+        {/*
           All the links in the footer should remain intact. You can delete the
           links only if you purchased the pro version. Licensing information:
           https://bootstrapmade.com/license/ Purchase the pro version with
           working PHP/AJAX contact form:
           https://bootstrapmade.com/delicious-free-restaurant-bootstrap-theme/
+        */}
+        <div className="credits">
           Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
         </div>
       </div>
