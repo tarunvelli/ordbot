d9b1766dcfdda0cb4a8dfe92d4ffba4aed69234a
Tarun <tarunvelli@gmail.com>
2020-09-27 11:24:13 +0530

    compression and meta schema

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module WhatsappRestaurantBot
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.middleware.use Rack::Deflater
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
