2bd9865a836a55d046eea97924d6416f50f00673
Tarun <tarunvelli@gmail.com>
2020-12-27 21:07:32 +0530

    add new help page

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,10 +1,12 @@
 class HelpController < ActionController::Base
   # TODO: replace with caches_page, requires actionpack-page_caching setup
-  # caches_action :index
+  caches_action :index
 
   def index; end
 
   def page
+    render status: 404 unless lookup_context.find_all("help/pages/#{params[:page]}").any?
+
     render template: "help/pages/#{params[:page]}"
   end
 end
